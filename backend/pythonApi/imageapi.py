021bce1fe0b01ab0dd8291cb4c6db5c44d2c8559
Om Prakash <om763616@gmail.com>
2023-04-05 23:02:26 +0530

    error handled

--- backend/pythonApi/imageapi.py
+++ backend/pythonApi/imageapi.py
@@ -3,8 +3,6 @@ import sys
 
 theInput=sys.argv[1]
 
-# print(theInput)
-
 Api = 'ZYMD9c0g7Hwk2uJ7lWSkAOjV4SHLlfzd'
 
 url = 'https://app.nanonets.com/api/v2/OCR/Model/25c077d9-2fb2-4b02-a639-1305fc2e6bb5/LabelFile/?async=false'
